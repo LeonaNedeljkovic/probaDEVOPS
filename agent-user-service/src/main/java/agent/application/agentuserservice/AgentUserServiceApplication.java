2012fd9c56ca126ff7e3d63ba9d77a2be7101fca
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-06-03 22:23:19 +0200

    vise srece ovaj put

---
+++ agent-user-service/src/main/java/agent/application/agentuserservice/AgentUserServiceApplication.java
@@ -0,0 +1,13 @@
+package agent.application.agentuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentUserServiceApplication.class, args);
+	}
+
+}
---
+++ agent-user-service/src/test/java/agent/application/agentuserservice/AgentUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package agent.application.agentuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
