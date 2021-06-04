5a14d8659aafa42208f666330855e5dbc80c3167
LeonaNedeljkovic <40694205+LeonaNedeljkovic@users.noreply.github.com>
2021-06-04 19:50:01 +0200

    Update AgentUserServiceApplication.java

--- agent-user-service/src/main/java/agent/application/agentuserservice/AgentUserServiceApplication.java
+++ agent-user-service/src/main/java/agent/application/agentuserservice/AgentUserServiceApplication.java
@@ -3,7 +3,7 @@ package agent.application.agentuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AgentUserServiceApplication {
 
 	public static void main(String[] args) {
